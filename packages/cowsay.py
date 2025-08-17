217d571ae98c64b89d0308a997487a7f0e3439a7
StefanTheFork <bozidardim838@gmail.com>
2025-08-17 17:38:55 +0300

    cowsay v1 has been released + hid some folders because they were ugly.

---
+++ packages/cowsay.py
@@ -0,0 +1,22 @@
+#cowsay v1 :fire:
+def say(args=None):
+    # if there aint no argument or its blank say moooooo
+    if args is None or (len(args) == 1 and args[0].strip() == ""):
+        print("the cow doesnt have a message to say...")
+        message = "moo"
+        length = len(message)
+    else:
+        message = ' '.join(args)
+        length = len(message)
+
+    print(' ' + '_' * (length + 2))
+    print(f'< {message} >')
+    print(' ' + '-' * (length + 2))
+    if args[1] is None:
+     print(r'''
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+                ''')
